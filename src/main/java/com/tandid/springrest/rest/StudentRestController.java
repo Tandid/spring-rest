230ce489d1dc56f44a0f9c509b9004c73574425c
Tandid Alam <tandid.alam@gmail.com>
2023-10-15 16:45:48 -0400

    Added Exception Handling

--- src/main/java/com/tandid/springrest/rest/StudentRestController.java
+++ src/main/java/com/tandid/springrest/rest/StudentRestController.java
@@ -2,6 +2,8 @@ package com.tandid.springrest.rest;
 
 import com.tandid.springrest.entity.Student;
 import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -30,7 +32,23 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+
+        if ((studentId > theStudents.size()) || (studentId < 0)){
+            throw new StudentNotFoundException("Student id not found = " + studentId);
+        }
+
         return theStudents.get(studentId);
 
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
